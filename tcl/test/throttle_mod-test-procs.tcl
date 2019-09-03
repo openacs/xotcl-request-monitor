279ccee79744f16ed2e1a2ed56ecdb0be08240b0
Hector Romojaro <hector.romojaro@gmail.com>
2019-09-03 06:35:55 +0000

    Remove unnecessary 'aa_run_with_teardown'

--- tcl/test/throttle_mod-test-procs.tcl
+++ tcl/test/throttle_mod-test-procs.tcl
@@ -14,18 +14,14 @@ aa_register_case \
 
         @author Hanifa Hasan
 } {
-    aa_run_with_teardown \
-        -rollback \
-        -test_code {
-            set full_string "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd"
-            aa_equals "Empty ellipsis short length" [string_truncate_middle -ellipsis "" -len 5 $full_string ] "sasd"
-            aa_equals "Empty ellipsis"              [string_truncate_middle -ellipsis "" $full_string ] "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd" 
-            aa_equals "No length"                   [string_truncate_middle -ellipsis ":::::::::::" $full_string ] "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd"
-            aa_equals "Short length"                [string_truncate_middle -ellipsis ":::::::::::" -len 10 $full_string ] "sadlf:::::::::::ydfsd"
-            aa_equals "Default options"             [string_truncate_middle $full_string ] "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd"
-            aa_equals "Length 0, default ellipsis"  [string_truncate_middle -len 0 $full_string ] "..."
-            aa_equals "Length 25, default ellipsis" [string_truncate_middle -len 25 $full_string ] "sadlfvnsaödc...4zbrefsydfsd"
-        }
+    set full_string "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd"
+    aa_equals "Empty ellipsis short length" [string_truncate_middle -ellipsis "" -len 5 $full_string ] "sasd"
+    aa_equals "Empty ellipsis"              [string_truncate_middle -ellipsis "" $full_string ] "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd" 
+    aa_equals "No length"                   [string_truncate_middle -ellipsis ":::::::::::" $full_string ] "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd"
+    aa_equals "Short length"                [string_truncate_middle -ellipsis ":::::::::::" -len 10 $full_string ] "sadlf:::::::::::ydfsd"
+    aa_equals "Default options"             [string_truncate_middle $full_string ] "sadlfvnsaödcösalcmksadöldcmsasdcsadvwaef4q3t54zbrefsydfsd"
+    aa_equals "Length 0, default ellipsis"  [string_truncate_middle -len 0 $full_string ] "..."
+    aa_equals "Length 25, default ellipsis" [string_truncate_middle -len 25 $full_string ] "sadlfvnsaödc...4zbrefsydfsd"
 }
 
 # Local variables:
