ccedd433fb5cbc824c8dacc01150a64d65117201
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2021-03-22 08:25:52 +0000

    restrict permissions of ip-info page

--- www/ip-info.tcl
+++ www/ip-info.tcl
@@ -14,7 +14,16 @@ ad_page_contract {
 set title "IP Lookup"
 set context [list $title]
 
-if {[catch {set dns_name [ns_hostbyaddr $ip]}]} { set dns_name "DNS lookup for $ip failed" }
+set admin_p [acs_user::site_wide_admin_p]
+if {!$admin_p} {
+  ad_return_warning "Insufficient Permissions" \
+      "Only side wide admins are allowed to view this page!"
+  ad_script_abort
+}
+
+if {[catch {set dns_name [ns_hostbyaddr $ip]}]} {
+  set dns_name "DNS lookup for $ip failed"
+}
 
 
 # Local variables:
